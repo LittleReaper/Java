7695903c29c450df29501e4514df5e2fd36169fd
LittleReaper <162659840+LittleReaper@users.noreply.github.com>
2024-11-21 11:05:43 -0600

    my first changes

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // single line comment in java
+
+        System.out.println("Jon Cline!");
+
+        //JOptionPane.showMessageDialog(null, "This is Graphical Output");
+
+        System.out.println("Your random is: " + (1+ (int) (Math.random() * 10)));
+    }
+}
